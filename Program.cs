6d8ef786906372489cf7f8cd371768639d4d4048
GcellyGomes <gicelly.gomes@etec.sp.gov.br>
2023-11-26 20:25:21 -0300

    concluido

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//Exercicio EuSouGroot - GicellyGomes
+﻿string FraseParaSair = "tchau";
+
+Console.WriteLine("--- Groot Chatbot ---\n");
+
+string pergunta;
+do
+{
+    Console.Write("Pergunta: ");
+    pergunta = Console.ReadLine()!;
+
+    string resposta = ObtemRespostaGroot(pergunta);
+
+    ExibeResposta(ObtemRespostaGroot(pergunta));
+
+}
+while (pergunta.Trim().ToLower() != FraseParaSair);
+
+void ExibeResposta(string resposta)
+{
+    Console.WriteLine($"            Resposta: {resposta}");
+}
+
+string ObtemRespostaGroot(string pergunta)
+{
+    return pergunta.Trim().ToLower() == FraseParaSair ?
+        "Eu sou Groot!" :
+        "Eu sou Groot.";
+}
